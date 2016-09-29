cbe42edf9b4737d7359ba542a3b352e423357da1
johnthagen <johnthagen@gmail.com>
2016-09-29 15:53:56 -0400

    Add Python 3.3 and 3.4 support.

--- setup.py
+++ setup.py
@@ -26,6 +26,8 @@ setuptools.setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
 )
