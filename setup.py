88b149e0605006a0ddc3c02e6497284b177238b9
David Hagen <david@drhagen.com>
2016-11-16 05:51:35 -0500

    Bump version number to 1.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setuptools.setup(
     name='parsita',
-    version='1.0.0',
+    version='1.1.0',
 
     description='Parser combinator library for Python.',
     long_description=open('README.rst').read(),
