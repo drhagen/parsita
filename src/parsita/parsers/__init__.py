32600029d415136cf93fe7a7baa071893f8b4b7d
David Hagen <david@drhagen.com>
2024-11-26 13:57:56 -0500

    Re-export all public members for mypy

--- src/parsita/parsers/__init__.py
+++ src/parsita/parsers/__init__.py
@@ -1,20 +1,43 @@
-from ._alternative import FirstAlternativeParser, LongestAlternativeParser, first, longest
-from ._any import AnyParser, any1
-from ._base import Parser, wrap_literal
-from ._conversion import ConversionParser, TransformationParser
-from ._debug import DebugParser, debug
-from ._end_of_source import EndOfSourceParser, eof
-from ._literal import LiteralParser, lit
-from ._optional import OptionalParser, opt
-from ._predicate import PredicateParser, pred
-from ._regex import RegexParser, reg
-from ._repeated import RepeatedOnceParser, RepeatedParser, rep, rep1
-from ._repeated_seperated import (
-    RepeatedOnceSeparatedParser,
-    RepeatedSeparatedParser,
-    rep1sep,
-    repsep,
+# Use `as` to mark names as re-exports from submodules for mypy.
+from ._alternative import (
+    FirstAlternativeParser as FirstAlternativeParser,
+    LongestAlternativeParser as LongestAlternativeParser,
+    first as first,
+    longest as longest,
 )
-from ._sequential import DiscardLeftParser, DiscardRightParser, SequentialParser
-from ._success import FailureParser, SuccessParser, failure, success
-from ._until import UntilParser, until
+from ._any import AnyParser as AnyParser, any1 as any1
+from ._base import Parser as Parser, wrap_literal as wrap_literal
+from ._conversion import (
+    ConversionParser as ConversionParser,
+    TransformationParser as TransformationParser,
+)
+from ._debug import DebugParser as DebugParser, debug as debug
+from ._end_of_source import EndOfSourceParser as EndOfSourceParser, eof as eof
+from ._literal import LiteralParser as LiteralParser, lit as lit
+from ._optional import OptionalParser as OptionalParser, opt as opt
+from ._predicate import PredicateParser as PredicateParser, pred as pred
+from ._regex import RegexParser as RegexParser, reg as reg
+from ._repeated import (
+    RepeatedOnceParser as RepeatedOnceParser,
+    RepeatedParser as RepeatedParser,
+    rep as rep,
+    rep1 as rep1,
+)
+from ._repeated_seperated import (
+    RepeatedOnceSeparatedParser as RepeatedOnceSeparatedParser,
+    RepeatedSeparatedParser as RepeatedSeparatedParser,
+    rep1sep as rep1sep,
+    repsep as repsep,
+)
+from ._sequential import (
+    DiscardLeftParser as DiscardLeftParser,
+    DiscardRightParser as DiscardRightParser,
+    SequentialParser as SequentialParser,
+)
+from ._success import (
+    FailureParser as FailureParser,
+    SuccessParser as SuccessParser,
+    failure as failure,
+    success as success,
+)
+from ._until import UntilParser as UntilParser, until as until
--- src/parsita/state/__init__.py
+++ src/parsita/state/__init__.py
@@ -1,4 +1,15 @@
-from ._exceptions import ParseError, RecursionError
-from ._reader import Reader, SequenceReader, StringReader
-from ._result import Failure, Result, Success
-from ._state import Continue, Element, Input, Output, State
+# Use `as` to mark names as re-exports from submodules for mypy.
+from ._exceptions import ParseError as ParseError, RecursionError as RecursionError
+from ._reader import (
+    Reader as Reader,
+    SequenceReader as SequenceReader,
+    StringReader as StringReader,
+)
+from ._result import Failure as Failure, Result as Result, Success as Success
+from ._state import (
+    Continue as Continue,
+    Element as Element,
+    Input as Input,
+    Output as Output,
+    State as State,
+)
